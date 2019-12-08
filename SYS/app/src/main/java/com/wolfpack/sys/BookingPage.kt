3427fedf40bdbb313ca97739c72043a8cb619f1b
SamuraiWarmonger <misc.samchris@gmail.com>
2019-12-08 07:08:38 +0530

    Update

--- SYS/app/src/main/java/com/wolfpack/sys/BookingPage.kt
+++ SYS/app/src/main/java/com/wolfpack/sys/BookingPage.kt
@@ -2,12 +2,56 @@ package com.wolfpack.sys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_booking_page.*
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.util.*
+import android.widget.TimePicker
+import android.app.TimePickerDialog
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
 
 class BookingPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_booking_page)
+        Date.setOnClickListener {
+            val c = Calendar.getInstance()
+            val mYear = c.get(Calendar.YEAR)
+           val  mMonth = c.get(Calendar.MONTH)
+           val mDay = c.get(Calendar.DAY_OF_MONTH)
 
+
+            val datePickerDialog = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    Date.setText(
+                        dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
+                    )
+                }, mYear, mMonth, mDay
+            )
+            datePickerDialog.show()
+        }
+        Time.setOnClickListener {
+            val c = Calendar.getInstance()
+            val mHour = c.get(Calendar.HOUR_OF_DAY)
+            val mMinute = c.get(Calendar.MINUTE)
+
+            // Launch Time Picker Dialog
+            val timePickerDialog = TimePickerDialog(this,
+                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                    Time.setText(
+                        "$hourOfDay:$minute"
+                    )
+                }, mHour, mMinute, false
+            )
+            timePickerDialog.show()
+        }
     }
 }
